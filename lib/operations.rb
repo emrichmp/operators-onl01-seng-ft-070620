26813fe541b7010b405ec296b3c429693c6e959b
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-07 21:20:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if 60 > speed > 40
+  if 60 < speed < 40
     return true
   else
     return false
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	60 > speed > 40 ? true : false
+	60 < speed < 40 ? true : false
 end
 	
 
